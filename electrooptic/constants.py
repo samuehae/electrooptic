27384019d044c0cc4b54800e8bb2cec2b75cae2d
Samuel Haeusler <samuehae@users.noreply.github.com>
2025-04-29 16:19:41 +0200

    add module with useful constants

---
+++ electrooptic/constants.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+'''physical and mathematical constants.'''
+
+
+# characteristic impedance of vacuum, in ohm
+Z_vac = 376.730313668 # CODATA 2018
+
+
+# speed of light in vaccum, in m/s
+c_s = 2.99792458e8 # CODATA 2018
+
+
+# vacuum electric permittivity, in F/m
+eps_0 = 8.8541878128e-12 # CODATA 2018
+
+
+# vacuum magnetic permeability, in H/m
+mu_0 = 1.25663706212e-6 # CODATA 2018
+
+
+
+# high numerical value handles mathematical singularities
+INF = 1e100
